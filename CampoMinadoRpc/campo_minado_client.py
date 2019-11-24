295360264ef357ef6f952b71d54fc093360f499a
Wesley <wsantos.inbox@gmail.com>
2019-11-24 14:37:28 -0300

    Criando a lógica do client

--- CampoMinadoRpc/campo_minado_client.py
+++ CampoMinadoRpc/campo_minado_client.py
@@ -1,9 +1,46 @@
 import time
 import rpyc
+import sys
 
-def testConection():
+def display_campo_minado(self):
+    for display in self.display:
+        print(display)
+
+def start_game():
     config = {'allow_public_attrs': True}
     cm = rpyc.connect('localhost', 8080, config=config)
-    cm.root.funfou()
 
+    while True:
+        tabuleiro = cm.root.get_tabuleiro()
+        print(tabuleiro)
 
+        print('\nFaça sua jogada: \n')
+        linha = input('Escolha a linha:')
+        coluna = input('Escolha a coluna:')
+        cm.root.jogada(linha=int(linha),coluna=int(coluna))
+
+        if cm.root.get_coordenada_valida():
+            if cm.root.get_jogada_segura():
+                cm.root.get_tabuleiro()
+            elif cm.root.get_game_over():
+                print("Você acertou uma bomba ! \n")
+                cm.root.get_tabuleiro()
+        else:
+            print("Jogada inválida !")        
+        
+def client():
+    while True:
+        print('###### Menu ######:\n')
+        print('###### 1 - INICIAR JOGO ######\n')
+        print('###### 2 - Sair ######')
+        opcao = input('O que você me conta ? 1 ou 2 ? \n ')
+        if opcao == '1':
+            start_game()
+            continue
+        elif opcao == '2':
+            print('BYE')
+            sys.exit(0)
+            break
+        else:
+            print('Entrada inválida')
+            continue
